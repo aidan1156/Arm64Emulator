345a30ff4b2f65d0ff679cfa1c5a7f80caa1cd4e
Aidan Baker <ab2923@ray01.doc.ic.ac.uk>
2024-05-25 17:27:55 +0100

    idk how to structure c stuff

--- src/emulate.c
+++ src/emulate.c
@@ -4,7 +4,7 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-#include "./emulator/machine.c"
+// #include "./emulator/machine.h"
 #include "./emulator/memory.c"
 
 
@@ -33,8 +33,22 @@ int main(int argc, char **argv) {
     loadProgram(argv[1], &machine);
 
     uint32_t currentInstruction;
+    int op0;
     for (int i=0;i<10;i++){
         currentInstruction = fetchInstruction(&machine);
+
+        //determine what of the 4 types the instr is
+        op0 = (currentInstruction >> 25) & 0xf;
+        if (op0 & 0xd == 8) { // 0xd == 0b1110
+            // it is Data Processing (Immediate)
+        } else if (op0 & 0x7 == 5) {
+            // it is Data Processing (Register) 
+        } else if (op0 & 0x5 == 4) {
+            // it is Loads and Stores
+        } else if (op0 & 0xd == 10) {
+            // it is Branches
+        }
+        printBinary(op0, 4);
         printBinary(currentInstruction, 32);
 
         machine.PC += 4;
