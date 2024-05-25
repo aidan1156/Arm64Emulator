42871ba3b9be6e2ecdae21282a2eb40f88f5262f
Aidan Baker <ab2923@ray01.doc.ic.ac.uk>
2024-05-25 22:59:57 +0100

    removed extra ;

--- src/emulate.c
+++ src/emulate.c
@@ -13,7 +13,7 @@
 
 // print binary number
 void printBinary(uint32_t n, int nbits) {
-    uint32_t mask = 1 << (nbits - 1);;
+    uint32_t mask = 1 << (nbits - 1);
     for (int i=0; i<nbits; i++) {
         if (i % 8 == 0) {
             putchar(' ');
