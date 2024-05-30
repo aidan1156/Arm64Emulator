911c9f0c111a253f45d41e40c0dc9fba3a62b2bd
ab2923 <ab2923@ic.ac.uk>
2024-05-30 14:40:12 +0100

    tidy up of codeready for next part

--- src/assemble.c
+++ src/assemble.c
@@ -4,28 +4,16 @@
 
 #include "./assembler/maps.h"
 
-void findLabels(Map* map, char* path) {
-    FILE *file = fopen(path, "r");
 
-    int c = fgetc(file);
+char* readLine(FILE *file) {
     int maxSize = 50; // max nume of elements the buffer can hold, do not do less than 3!!!!!
     char* currentInstr = malloc(maxSize * sizeof(char)); // we dont know the max length of a label or line so malloc it
     currentInstr[0] = '\0';
-    int currentAddress = 0;
+
+    int c = fgetc(file);
     while (c != EOF) {
         if (c == '\n') {
-            // if the final character in the instruction is a ':' then its a label
-            if (currentInstr[strlen(currentInstr) - 1] == ':') {
-                // remove the ':' so we have just the label
-                currentInstr[strlen(currentInstr) - 1] = '\0';
-                // copy the label into something that wont get overwritten
-                char* label = malloc((strlen(currentInstr) + 1) * sizeof(char));
-                strcpy(label, currentInstr);
-                insertMap(map, label, currentAddress + 4);
-            } else { // otherwise increment the address
-                currentAddress += 4;
-            }
-            currentInstr[0] = '\0'; // clear the buffer
+            return currentInstr;
         } else {
             // the instruction is FAT so make our buffer fatter
             if (strlen(currentInstr) + 1 >= maxSize) {
@@ -42,9 +30,35 @@ void findLabels(Map* map, char* path) {
         }
         c = fgetc(file);
     }
+
+    return NULL;
 }
 
+
+void findLabels(Map* map, char* path) {
+    FILE *file = fopen(path, "r");
+    char* currentInstr = readLine(file);
+    int currentAddress = 0;
+    while (currentInstr != NULL) {
+        // if the final character in the instruction is a ':' then its a label
+        if (currentInstr[strlen(currentInstr) - 1] == ':') {
+            // remove the ':' so we have just the label
+            currentInstr[strlen(currentInstr) - 1] = '\0';
+            insertMap(map, currentInstr, currentAddress + 4);
+        } else if (strcmp(currentInstr, "")) { // otherwise increment the address
+            currentAddress += 4;
+        }
+        currentInstr = readLine(file);
+    }
+
+    fclose(file);
+}
+
+
 int main(int argc, char **argv) {
+    if (argc < 3) {
+        fprintf(stderr, "too few arguments supplied\n");
+    }
     // find the address
     Map* map = createMap(64);
     findLabels(map, argv[1]);
