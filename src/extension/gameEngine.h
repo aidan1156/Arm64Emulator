7a95f3d1de9caf3af58afa85ddc9e726fce36b84
ab2923 <ab2923@ic.ac.uk>
2024-06-15 22:41:52 +0100

    cleaned up imports

--- src/extension/gameEngine.h
+++ src/extension/gameEngine.h
@@ -1,12 +1,5 @@
 
-
-#include <termios.h> // used to hide terminal input
-#include <unistd.h> // also used to hide terminal input
-#include <pthread.h> // used to create a thread specifically to get input from terminal
-#include <stdio.h> 
 #include <stdbool.h>
-#include <assert.h>
-#include <stdlib.h>
 
 
 typedef struct Window *Window;
