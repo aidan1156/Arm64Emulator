33f04e312685dcc99cd689d4e9aab545019ec0a0
Yeona Kim <yk523@oak08.doc.ic.ac.uk>
2024-06-16 11:13:38 +0100

    attempting turn on and turn off immediately afterwards

--- programs/led_blink.s
+++ programs/led_blink.s
@@ -4,16 +4,24 @@ eor x1, x1, x1
 eor x2, x2, x2
 eor x3, x3, x3
 
-
+// making PIN3 as output
 ldr w0, d0
 add w1, w1, #0x2
 str w1 [w0, #1]
 
+// reset value of x1
 eor x1, x1, x1
 add w1, w1, #0x8
 
+// GPSET0 of PIN3 = 1
 str w1 [w0, #0x1c]
+
+// GPCLR0 PIN3 = 1
+str w1 [w0, #0x28]
+
+// termination
 and x0, x0, x0
+
 
 d0:
 .int 0x3f200000
