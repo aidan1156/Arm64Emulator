f7d532176560c1e82d32ccb331351c44a3de5906
Yeona Kim <yk523@oak08.doc.ic.ac.uk>
2024-06-16 19:11:53 +0100

    Added new code file + modified code with more directives

--- programs/copy.s
+++ programs/copy.s
@@ -9,18 +9,19 @@ address:
 .int 0x3f200000
 
 fsel:
-.int #0x200000
+.int 0x1000
 
 gpset:
-.int 0x80
+.int 0x4000
 
 ldr x0, address
 ldr x1, fsel
-str w1, [x0, #0]
+str w1, [x0, #4]
 
 ldr x2, gpset
 
+lmao:
 str w2, [x0, #0x1c]
-
-
+str w10, [x0, #0x1c]
+b lmao
 
