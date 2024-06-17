4c0a0af56dc6030e6d44d84a25766c81a3cfb4eb
Hridita Soni <hs4023@ash05.doc.ic.ac.uk>
2024-06-17 15:47:02 +0100

    blinker working yay

--- programs/led_blink.s
+++ programs/led_blink.s
@@ -1,9 +1,37 @@
 
-sub x0, x0, x0
-sub x1, x1, x1
-sub x2, x2, x2
-sub x3, x3, x3
-sub x4, x4, x4
+eor w0, w0, w0
+eor w1, w1, w1
+eor w2, w2, w2
+eor w3, w3, w3
+eor w4, w4, w4
+
+add w4, w4, #0x3ff lsl #12
+
+ldr w0, address
+ldr w1, fsel
+str w1, [x0, #0]
+
+ldr w2, gpset
+
+loop:
+str w2, [x0, #0x1c]
+eor w3, w3, w3
+
+delay1:
+add w3, w3, #1
+cmp w3, w4
+b.ne delay1
+
+str w2, [x0, #0x28]
+
+eor w3, w3, w3
+
+delay2:
+add w3, w3, #1
+cmp w3, w4
+b.ne delay2
+
+b loop
 
 address:
 .int 0x3f200000
@@ -13,14 +41,4 @@ fsel:
 
 gpset:
 .int 0x80
-
-ldr x0, address
-ldr x1, fsel
-str w1, [x0, #0]
-
-ldr x2, gpset
-
-str w2, [x0, #0x1c]
-
-
 
